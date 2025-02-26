60b4c13b8c5ffb7999fe0ff688c2a73876af3ab6
Mark Peters <mapeters101@earthlink.net>
2025-02-26 16:46:33 -0500

    Description for exercise01.

---
+++ exercise01.py
@@ -0,0 +1,23 @@
+"""
+Question 1
+Write some code that generates an m x n multiplication table.
+
+For example if m=3 and n=4 your output should look something like:
+
+1 x 1 = 1
+1 x 2 = 2
+1 x 3 = 3
+1 x 4 = 4
+---------------
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+---------------
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+---------------
+Your code should be generic enough that it can handle any positive integer values of m and n.
+"""
