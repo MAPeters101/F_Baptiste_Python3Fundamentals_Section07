9e1248d39051e67e84eed2af114005521557e8bf
Mark Peters <mapeters101@earthlink.net>
2025-02-26 16:47:00 -0500

    Description for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,17 @@
+"""
+Question 2
+Your are given the following tuple of lists:
+
+data = (
+    ['2021-01-01', 10, 20],
+    ['2021-01-02', 20, 18],
+    ['2021-01-03', -10, 10],
+    ['2021-01-04', 100, 102],
+    ['2021-01-05', 20, 45]
+)
+Your program should:
+
+Mutate the lists in data to add one more element indicating the distance between the two integer numbers (i.e. the absolute value fo the difference)
+Determine on which date this newly calculate value was the largest.
+Be able to work for a data set containing any number of lists.
+"""
