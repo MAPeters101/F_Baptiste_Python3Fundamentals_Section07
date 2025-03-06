7f37776c6b6e70a7259d344e939f4b1a3f90599f
Mark Peters <mapeters101@earthlink.net>
2025-03-06 15:16:02 -0500

    Complete work on lesson070 from 2:10.

--- lesson070.py
+++ lesson070.py
@@ -6,7 +6,17 @@ print(tuple('abc'))
 print(tuple(range(5)))
 print()
 
+print(list(range(3, 9)))
+print(len(range(3, 9)))
+print()
+print(len(range(10)))
+print()
 
+print(list(range(2,10,2)))
+print(list(range(2,11,2)))
+print(len(range(2,10,2)))
+print(len(range(2,11,2)))
+print()
 
 
 
