56456f44e2c1ebc04e880b14174ecd3e8d41b8d5
Mark Peters <mapeters101@earthlink.net>
2025-03-06 21:50:14 -0500

    Work on lesson072 from 2:00 to 4:40.

---
+++ lesson072.py
@@ -0,0 +1,18 @@
+
+suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+for suit in suits:
+    print(f'{suit[0].upper()} = {suit}')
+
+for c in 'python':
+    print(c)
+
+print(c, suit)
+print()
+
+for i in range(2, 11, 2):
+    print(i)
+
+print(list(range(2,11,2)))
+
+
+
