c31d2359a5e8a21994d13b1bb146a56ee3350165
Mark Peters <mapeters101@earthlink.net>
2025-03-06 21:58:53 -0500

    Work on lesson072 from 7:00 to 9:40.

--- lesson072.py
+++ lesson072.py
@@ -13,6 +13,24 @@ for i in range(2, 11, 2):
     print(i)
 
 print(list(range(2,11,2)))
+print('-'*80)
+
+for i in range(3):
+    for j in range(3):
+        print(f'i={i}, j={j}')
+    print('-'*10)
+print('-'*80)
+
+m = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+for row_idx in range(3):
+    for col_idx in range(3):
+        print(f'[{row_idx}, {col_idx}] = {m[row_idx][col_idx]}')
+print('='*80)
 
 
 
