a384c6994cf6c4a9008513d510f87b92a8f9c861
Mark Peters <mapeters101@earthlink.net>
2025-03-08 18:37:21 -0500

    Work on lesson074 from 4:15 to 5:40.

--- lesson074.py
+++ lesson074.py
@@ -11,6 +11,22 @@ price = 100
 while price < 50:
     print(f'price={price}')
 print('Done')
+print('-'*80)
+
+# price = 100
+# while price > 90:
+#     print(price)
+#     price += 1
+# print('Done')
+
+
+
+
+
+
+
+print('-'*80)
+
 
 
 
