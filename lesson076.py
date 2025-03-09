100b38b4599e25bdd291d0fef94a0481f630a83a
Mark Peters <mapeters101@earthlink.net>
2025-03-08 21:11:41 -0500

    Work on lesson076 from 4:15 to 7:50.

--- lesson076.py
+++ lesson076.py
@@ -15,7 +15,16 @@ print()
 for i in range(1,11):
     if i%2 == 0:
         print(i)
+print('-'*80)
 print()
+
+for i in range(1, 5):
+    for j in range(1, 5):
+        if (i + j) %2 == 1:
+            print(f'{i} + {j} is odd, skipping...')
+            continue
+        print(f'adding numbers: {i} + {j} = {i + j}')
+    print('-'*10)
 
 
 
