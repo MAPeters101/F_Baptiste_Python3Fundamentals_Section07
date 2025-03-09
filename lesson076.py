bf7b24a247fe61cb627fff2c6939c9a6e7ba2407
Mark Peters <mapeters101@earthlink.net>
2025-03-08 21:20:38 -0500

    Work on lesson076 from 11:10 to 13:50.

--- lesson076.py
+++ lesson076.py
@@ -43,5 +43,30 @@ while True:
     if i > 5:
         break
     print(i)
+print('-'*80)
+
+
+data = [1,2,3,-4,5,6]
+all_positive = True
+for element in data:
+    if element <= 0:
+        all_positive = False
+        break
+
+if all_positive:
+    print('Processing all positive elements')
+print('--')
+
+data = [1,2,3,4,5,6]
+all_positive = True
+for element in data:
+    if element <= 0:
+        all_positive = False
+        break
+
+if all_positive:
+    print('Processing all positive elements')
+
+
 
 
